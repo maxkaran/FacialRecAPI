e91f76820a1fff4539d43ce90869d0f49512235e
Max Karan <maxkaran2@gmail.com>
2018-12-10 17:40:08 -0500

    Set up database connection with mLab database

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('./config/db');
 
 const app            = express();
 
@@ -9,6 +10,18 @@ const port = 3000;
 //routing
 const APIroutes = require('./app/APIroutes');
 app.use('/api', APIroutes);
+
+//to handle url encoded content
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//set up database connection
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on ' + port);
+  });               
+})
 
 
 app.listen(port, function(){
